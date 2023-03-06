1ce4f16cc0f3a2fb49d5fbe92153e6e1fb96388e
Ankan5960 <104480693+Ankan5960@users.noreply.github.com>
2023-03-07 02:51:52 +0530

    Add files via upload

---
+++ pr.js
@@ -0,0 +1,7 @@
+function func(){
+    let email = document.getElementsByTagName('input')[0].value;
+    let password = document.getElementsByTagName('input')[1].value;
+
+    console.log("Username: "+email+", Password: "+ password);
+    confirm("log in scucessful");
+}
